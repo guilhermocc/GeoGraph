9357fce87ccdbe3c171529abd51d6f3d3467e592
Guilherme Carvalho <Guilhermbrsp@gmail.com>
2020-03-01 14:41:28 -0300

    Optimize markers and geolocation update.

--- lib/map.dart
+++ lib/map.dart
@@ -20,11 +20,13 @@ class _MapPageState extends State<MapPage> {
   final LatLng _center = LatLng(-23.563900, -46.653641);
   final Map<String, Marker> _markers = {};
   BitmapDescriptor pinLocationIcon;
-
   final geoLocator = Geolocator();
+  var locationHasBeenQueriedOnDataBase = false;
+  var locationExistsOnDataBase = false;
   final locationOptions = LocationOptions(
       accuracy: LocationAccuracy.high, distanceFilter: 1, timeInterval: 5000);
   StreamSubscription<Position> positionSubscription;
+  StreamSubscription<QuerySnapshot> refreshSubscription;
   GoogleMapController mapController;
 
   @override
@@ -37,6 +39,7 @@ class _MapPageState extends State<MapPage> {
   @override
   dispose() {
     positionSubscription.cancel();
+    refreshSubscription.cancel();
     super.dispose();
   }
 
@@ -45,6 +48,39 @@ class _MapPageState extends State<MapPage> {
     super.initState();
     setCustomMapPin();
     setPositionEventsSubscription();
+    setRefreshEventsSubscription();
+  }
+
+  void setRefreshEventsSubscription() {
+    refreshSubscription = Firestore.instance
+        .collectionGroup("markers")
+        .snapshots()
+        .listen((snapshot) {
+      refreshChangedMarkers(snapshot);
+    });
+  }
+
+  // This method is missing the logic to exclude the document of the self user,
+  // since the above query does not exclude the document change of the user itself,
+  // this logic should be implemented here. By the docs, there is no way to
+  // exclude a document in a query, so every time the user location is updated,
+  // this method is going to be triggered and the location of itself will be updated (again)
+  void refreshChangedMarkers(QuerySnapshot snapshot) {
+    Map<String, Marker> updatedMarkers = {};
+    snapshot.documentChanges.forEach((documentChange) {
+      var locationId = documentChange.document.documentID;
+      var newLatitude = documentChange.document.data["position"].latitude;
+      var newLongitude = documentChange.document.data["position"].longitude;
+      var newMarker = _markers[locationId]
+          .copyWith(positionParam: LatLng(newLatitude, newLongitude));
+      updatedMarkers[locationId] = newMarker;
+    });
+
+    setState(() {
+      updatedMarkers.forEach((String locationId, Marker newMarker) {
+        _markers[locationId] = newMarker;
+      });
+    });
   }
 
   void setCustomMapPin() async {
@@ -59,7 +95,7 @@ class _MapPageState extends State<MapPage> {
       print("*** GEOLOCATION UPDATE EVENT FIRED ***");
       FlutterRingtonePlayer.play(
           android: AndroidSounds.notification, ios: IosSounds.glass);
-      onPressUpdate();
+      updateGeoPointsAndRefreshSelfLocation(position);
     });
   }
 
@@ -67,26 +103,6 @@ class _MapPageState extends State<MapPage> {
     Widget personDialog = PersonDialog();
     showDialog(
         context: context, builder: (BuildContext context) => personDialog);
-  }
-
-  getLocationId() async {
-    return await Firestore.instance
-        .collection("users")
-        .document(widget.userId)
-        .collection("markers")
-        .limit(1)
-        .getDocuments()
-        .then((QuerySnapshot b) => b.documents.first.documentID);
-  }
-
-  locationExists() async {
-    return await Firestore.instance
-        .collection("users")
-        .document(widget.userId)
-        .collection("markers")
-        .limit(1)
-        .getDocuments()
-        .then((QuerySnapshot b) => b.documents.isEmpty ? false : true);
   }
 
   Future<Map<String, Marker>> getGroupMarkers(currentPosition) async {
@@ -117,11 +133,7 @@ class _MapPageState extends State<MapPage> {
   Future<void> _onMapCreated(GoogleMapController controller) async {
     Position currentPosition = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    if (await locationExists()) {
-      await updateGeoPoints(currentPosition);
-    } else {
-      createGeoPoints(currentPosition);
-    }
+    await createOrUpdateGeoPoint(currentPosition);
     var loadedMarkers = await getGroupMarkers(currentPosition);
 
     setState(() {
@@ -142,37 +154,6 @@ class _MapPageState extends State<MapPage> {
   }
 
   String capitalize(String s) => s[0].toUpperCase() + s.substring(1);
-
-  void createGeoPoints(Position currentPosition) async {
-    var currentUser = await getCurrentUser();
-
-    Firestore.instance
-        .collection("users")
-        .document(widget.userId)
-        .collection("markers")
-        .add({
-          "userName": capitalize(currentUser.data['fname']) +
-              " " +
-              capitalize(currentUser.data['surname']),
-          "position":
-              GeoPoint(currentPosition.latitude, currentPosition.longitude)
-        })
-        .then((result) => print("GEOPONTO ADICIONADO " + result.documentID))
-        .catchError((error) => print("ERRO AO ADICIONAR GEOPONTO" + error));
-  }
-
-  Future updateGeoPoints(Position currentPosition) async {
-    var locationId = await getLocationId();
-    Firestore.instance
-        .collection("users")
-        .document(widget.userId)
-        .collection("markers")
-        .document(locationId)
-        .updateData({
-      "position": GeoPoint(currentPosition.latitude, currentPosition.longitude)
-    });
-    print("GEOPONTO ATUALIZADO");
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -208,16 +189,97 @@ class _MapPageState extends State<MapPage> {
   void onPressUpdate() async {
     Position currentPosition = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    if (await locationExists()) {
-      await updateGeoPoints(currentPosition);
-    } else {
-      createGeoPoints(currentPosition);
-    }
+
+    await createOrUpdateGeoPoint(currentPosition);
     var loadedMarkers = await getGroupMarkers(currentPosition);
 
     setState(() {
       _markers.clear();
       _markers.addAll(loadedMarkers);
+    });
+  }
+
+  Future createOrUpdateGeoPoint(Position currentPosition) async {
+    if (await geoPointsExists()) {
+      await updateGeoPoints(currentPosition);
+    } else {
+      createGeoPoints(currentPosition);
+    }
+  }
+
+  void createGeoPoints(Position currentPosition) async {
+    var currentUser = await getCurrentUser();
+
+    Firestore.instance
+        .collection("users")
+        .document(widget.userId)
+        .collection("markers")
+        .add({
+          "userName": capitalize(currentUser.data['fname']) +
+              " " +
+              capitalize(currentUser.data['surname']),
+          "position":
+              GeoPoint(currentPosition.latitude, currentPosition.longitude)
+        })
+        .then((result) => print("GEOPOINT CREATED " + result.documentID))
+        .catchError((error) => print("ERROR WHILE CREATING GEOPOINT" + error));
+
+    locationExistsOnDataBase = true;
+  }
+
+  Future updateGeoPoints(Position currentPosition) async {
+    var locationId = await getLocationId();
+    if (await geoPointsExists()) {
+      Firestore.instance
+          .collection("users")
+          .document(widget.userId)
+          .collection("markers")
+          .document(locationId)
+          .updateData({
+        "position":
+            GeoPoint(currentPosition.latitude, currentPosition.longitude)
+      });
+    }
+    print("GEOPONTO ATUALIZADO");
+  }
+
+  getLocationId() async {
+    return await Firestore.instance
+        .collection("users")
+        .document(widget.userId)
+        .collection("markers")
+        .limit(1)
+        .getDocuments()
+        .then((QuerySnapshot b) => b.documents.first.documentID);
+  }
+
+  geoPointsExists() async {
+    if (!locationHasBeenQueriedOnDataBase) {
+      locationExistsOnDataBase = await Firestore.instance
+          .collection("users")
+          .document(widget.userId)
+          .collection("markers")
+          .limit(1)
+          .getDocuments()
+          .then((QuerySnapshot b) => b.documents.isEmpty ? false : true);
+      locationHasBeenQueriedOnDataBase = true;
+    }
+    return locationExistsOnDataBase;
+  }
+
+  Future<void> updateGeoPointsAndRefreshSelfLocation(
+      Position currentPosition) async {
+    updateGeoPoints(currentPosition);
+    var locationId = await getLocationId();
+    refreshSelfLocation(currentPosition, locationId);
+  }
+
+  void refreshSelfLocation(Position currentPosition, String locationId) {
+    var newMarker = _markers[locationId].copyWith(
+        positionParam:
+            LatLng(currentPosition.latitude, currentPosition.longitude));
+    setState(() {
+      _markers[locationId] = newMarker;
     });
   }
 }
