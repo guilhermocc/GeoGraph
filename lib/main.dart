22005ffc36761e2a99234df7961488e3438cbd88
Guilherme Carvalho <guilhermbrsp@gmail.com>
2020-05-17 22:30:42 -0300

    Add primary color pallete

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,11 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: 'Flutter Demo',
-        theme: ThemeData(primarySwatch: Colors.cyan),
+        theme: ThemeData(
+            primaryColor: Color(0xFF6200EE),
+            primaryColorLight: Color(0xFF9e47ff),
+            primaryColorDark: Color(0xFF0400ba)
+        ),
         home: SplashPage(),
         routes: <String, WidgetBuilder>{
           '/home': (BuildContext context) => HomePage(title: 'Home'),
