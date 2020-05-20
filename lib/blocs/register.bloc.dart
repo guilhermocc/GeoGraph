e423b47cfe7d37aa1e7e05b5de4c7c3521d8e553
Guilherme Carvalho <guilhermbrsp@gmail.com>
2020-05-19 22:52:54 -0300

    Create register bloc and refactor register page
    By using the bloc pattern we can isolate the business logic from visual components.

---
+++ lib/blocs/register.bloc.dart
@@ -0,0 +1,96 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:geograph/android/pages/home.dart';
+
+class RegisterBloc {
+  final GlobalKey<FormState> registerFormKey = GlobalKey<FormState>();
+  TextEditingController firstNameInputController = new TextEditingController();
+  TextEditingController lastNameInputController = new TextEditingController();
+  TextEditingController emailInputController = new TextEditingController();
+  TextEditingController passwordInputController = new TextEditingController();
+  TextEditingController confirmPasswordInputController =
+      new TextEditingController();
+  bool isLoading = false;
+
+  createAccount(BuildContext context) async {
+    if (registerFormKey.currentState.validate()) {
+      if (this.passwordInputController.text ==
+          this.confirmPasswordInputController.text) {
+        this.isLoading = true;
+        AuthResult authResult = await createUserWithEmailAndPassword();
+        await updateUserData(authResult);
+
+        Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(
+                builder: (context) => HomePage(
+                      // TODO this thitle is not beign displayed on home page
+                      title: this.firstNameInputController.text,
+                      uid: authResult.user.uid,
+                    )),
+            (_) => false);
+        this.firstNameInputController.clear();
+        this.lastNameInputController.clear();
+        this.emailInputController.clear();
+        this.passwordInputController.clear();
+        this.confirmPasswordInputController.clear();
+      } else {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Erro"),
+                content: Text("As senhas não conferem."),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text("Fechar"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  )
+                ],
+              );
+            });
+      }
+    }
+  }
+
+  createUserWithEmailAndPassword() async {
+    return FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: this.emailInputController.text,
+        password: this.passwordInputController.text);
+  }
+
+  updateUserData(AuthResult currentUser) async {
+    return Firestore.instance
+        .collection("users")
+        .document(currentUser.user.uid)
+        .setData({
+      "uid": currentUser.user.uid,
+      "fname": this.firstNameInputController.text,
+      "surname": this.lastNameInputController.text,
+      "email": this.emailInputController.text,
+    });
+  }
+
+  String emailValidator(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value)) {
+      return 'O Email está em um formato inválido';
+    } else {
+      return null;
+    }
+  }
+
+  String pwdValidator(String value) {
+    if (value.length < 8) {
+      return 'A Senha deve ter ao menos 8 caracteres';
+    } else {
+      return null;
+    }
+  }
+}
