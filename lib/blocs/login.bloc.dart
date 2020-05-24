b554137eadcd3772bce07f3ac6c57ad1ff2d4263
Guilherme Carvalho <guilhermbrsp@gmail.com>
2020-05-24 11:34:08 -0300

    Use user provider in login bloc and home screen

--- lib/blocs/login.bloc.dart
+++ lib/blocs/login.bloc.dart
@@ -5,9 +5,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:geograph/android/pages/home.dart';
+import 'package:geograph/blocs/user.bloc.dart';
+import 'package:geograph/store/user/user.dart';
 
 class LoginBloc {
   final GlobalKey<FormState> loginFormKey = GlobalKey<FormState>();
+  final UserBloc userBloc = UserBloc();
   var isLoading = false;
   var emailInputController = new TextEditingController();
   var passwordInputController = new TextEditingController();
@@ -40,6 +43,7 @@ class LoginBloc {
       this.isLoading = true;
       AuthResult authResult = await signInWithLoginInfo();
       DocumentSnapshot userSnapshot = await getUserInfo(authResult);
+      loadUserInfo(userSnapshot, context);
       navigateToHomePage(userSnapshot, context);
     }
   }
@@ -69,7 +73,7 @@ class LoginBloc {
                 ))).catchError((err) => this.isLoading = false);
   }
 
-  handleLoginError(PlatformException err, BuildContext context) {
+  handleLoginError(err, BuildContext context) {
     this.isLoading = false;
     if (err.code == "ERROR_WRONG_PASSWORD" ||
         err.code == "ERROR_USER_NOT_FOUND") {
@@ -98,5 +102,9 @@ class LoginBloc {
             ],
           );
         });
+  }
+
+  void loadUserInfo(DocumentSnapshot userSnapShot, BuildContext context) {
+    userBloc.updateUserStore(userSnapShot, context);
   }
 }
