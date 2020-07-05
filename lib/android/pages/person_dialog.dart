ccf72bd58599c0994daae47fc79cc6f69fbae652
Guilherme Carvalho <guilhermbrsp@gmail.com>
2020-07-05 17:03:34 -0300

    Create delete member in person dialog improved

--- lib/android/pages/person_dialog.dart
+++ lib/android/pages/person_dialog.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
-class PersonDialog extends StatelessWidget {
+class PersonDialog extends StatefulWidget {
   PersonDialog(
       {Key key,
       this.placemark,
@@ -21,117 +21,185 @@ class PersonDialog extends StatelessWidget {
   final groupUid;
   final String controllerUserType;
 
+  @override
+  _PersonDialogState createState() => _PersonDialogState();
+}
+
+class _PersonDialogState extends State<PersonDialog> {
+  bool deleteSuccessful = true;
+  bool triedToDelete = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return triedToDelete
+        ? Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Container(
+              height: 300.0,
+              width: 300.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          deleteSuccessful
+                              ? 'Usuário deletado com sucesso'
+                              : "Erro ao remover membro, tente novamente.",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, top: 50),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(
+                            'Fechar',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        : Dialog(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: Container(
+              height: 300.0,
+              width: 300.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          'Nome: ${widget.username}',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        widget.placemark.administrativeArea != ""
+                            ? Text(
+                                'Estado: ${widget.placemark.administrativeArea}',
+                                style: TextStyle(color: Colors.black),
+                              )
+                            : Container(),
+                        widget.placemark.subAdministrativeArea != ""
+                            ? Text(
+                                'Cidade: ${widget.placemark.subAdministrativeArea}',
+                                style: TextStyle(color: Colors.black),
+                              )
+                            : Container(),
+                        widget.placemark.subLocality != ""
+                            ? Text(
+                                'Bairro: ${widget.placemark.subLocality}',
+                                style: TextStyle(color: Colors.black),
+                              )
+                            : Container(),
+                        widget.placemark.thoroughfare != ""
+                            ? Text(
+                                'Rua: ${widget.placemark.thoroughfare}',
+                                style: TextStyle(color: Colors.black),
+                              )
+                            : Container(),
+                        widget.placemark.subThoroughfare != ""
+                            ? Text(
+                                'Número: ${widget.placemark.subThoroughfare}',
+                                style: TextStyle(color: Colors.black),
+                              )
+                            : Container(),
+                        widget.formatedDistance != ""
+                            ? Text(
+                                'Distância: ${widget.formatedDistance}',
+                                style: TextStyle(color: Colors.black),
+                              )
+                            : Container(),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 10, right: 10, top: 50),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        RaisedButton(
+                          color: Theme.of(context).primaryColorDark,
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(
+                            'Fechar',
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                        ),
+                        widget.controllerUserType == "admin"
+                            ? RaisedButton(
+                                color: Theme.of(context).primaryColorLight,
+                                onPressed: () async {
+                                  bool deleteStatus = true;
+                                  await removeGroupMember().catchError((error) {
+                                    deleteStatus = false;
+                                  });
+                                  setState(() {
+                                    triedToDelete = true;
+                                    deleteSuccessful = deleteStatus;
+                                  });
+                                },
+                                child: Text(
+                                  'Excluir membro',
+                                  style: TextStyle(
+                                      fontSize: 18.0, color: Colors.white),
+                                ),
+                              )
+                            : Container()
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+  }
+
   Future<void> removeGroupMember() async {
     CollectionReference groupReference =
         Firestore.instance.collection("groups");
     DocumentReference userReference =
-        Firestore.instance.collection("users").document(memberUid);
-    DocumentReference groupDocumentReference =
-        Firestore.instance.collection("groups").document(groupUid);
+        Firestore.instance.collection("users").document(widget.memberUid);
+    DocumentReference groupDocumentReference = Firestore.instance
+        .collection("groups")
+        .document(widget.groupUid);
 
     DocumentSnapshot snapshot = await groupDocumentReference.get();
     List<dynamic> members = snapshot.data["members"];
-    members.removeWhere((member) => member["uid"].documentID == memberUid);
+    members
+        .removeWhere((member) => member["uid"].documentID == widget.memberUid);
 
     groupDocumentReference.updateData({"members": members});
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: Container(
-        height: 300.0,
-        width: 300.0,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    'Nome: ${username}',
-                    style: TextStyle(color: Colors.black),
-                  ),
-                  placemark.administrativeArea != ""
-                      ? Text(
-                          'Estado: ${placemark.administrativeArea}',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      : Container(),
-                  placemark.subAdministrativeArea != ""
-                      ? Text(
-                          'Cidade: ${placemark.subAdministrativeArea}',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      : Container(),
-                  placemark.subLocality != ""
-                      ? Text(
-                          'Bairro: ${placemark.subLocality}',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      : Container(),
-                  placemark.thoroughfare != ""
-                      ? Text(
-                          'Rua: ${placemark.thoroughfare}',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      : Container(),
-                  placemark.subThoroughfare != ""
-                      ? Text(
-                          'Número: ${placemark.subThoroughfare}',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      : Container(),
-                  formatedDistance != ""
-                      ? Text(
-                          'Distância: ${formatedDistance}',
-                          style: TextStyle(color: Colors.black),
-                        )
-                      : Container(),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 10, right: 10, top: 50),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  RaisedButton(
-                    color: Theme.of(context).primaryColorDark,
-                    onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Text(
-                      'Fechar',
-                      style: TextStyle(fontSize: 18.0, color: Colors.white),
-                    ),
-                  ),
-                  controllerUserType == "admin"
-                      ? RaisedButton(
-                          color: Theme.of(context).primaryColorLight,
-                          onPressed: () async {
-                            await removeGroupMember();
-                          },
-                          child: Text(
-                            'Excluir membro',
-                            style:
-                                TextStyle(fontSize: 18.0, color: Colors.white),
-                          ),
-                        )
-                      : Container()
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
   }
 }
